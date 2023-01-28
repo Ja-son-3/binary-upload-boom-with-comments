61a5cc456c889af1fa97bf01500d669344644293
Jason Huang <jason97521@gmail.com>
2023-01-28 00:47:34 -0500

    Username and Delete added to comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,4 +1,5 @@
-const Comment = require('../models/Comment')
+const Comment = require('../models/Comment');
+const Post = require('../models/Post');
 
 module.exports = {
   createComment: async (req, res) => {
@@ -7,11 +8,21 @@ module.exports = {
         comment: req.body.comment,
         likes: 0,
         post: req.params.id,
+        createdBy: req.user.userName,
+        createdById: req.user.id,
       });
       console.log("Comment has been added!");
       res.redirect("/post/"+req.params.id);
     } catch (err) {
       console.log(err);
+    }
+  },
+  deleteComments: async (req, res) => {
+    try {
+      await Comment.deleteOne({_id: req.params.commentid})
+      res.redirect("/post/"+req.params.postid)
+    } catch (err) {
+      console.log(err)
     }
   },
 };
